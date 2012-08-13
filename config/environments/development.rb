3dcab1af77cffd4c33eb6745fcc24471ccbcccfe
Chris Hill <christopherbrucehill@gmail.com>
2012-08-12 17:52:41 -0700

    Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,21 @@ Depot::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Don't actually send emails
+  # config.action_mailer.delivery_method = :test
+  #
+  # Alternate configuration example, using gmail:
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:        "smtp.gmail.com",
+      port:           587,
+      domain:         "gmail.com",
+      authentication: "plain",
+      user_name:      "christopherbrucehill@gmail.com",
+      password:       "Hockey#22",
+      enable_starttls_auto: true
+    }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
